a1f6ae25a4cd28e745662f174a5ce3ea0708a7bc
hehe <2681560676@qq.com>
2023-05-22 15:53:56 +0800

    静态资源设置和访问

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -37,7 +37,6 @@ DEBUG = True
 # 默认是127.0.0.1
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 # 注册安装子应用
 INSTALLED_APPS = [
@@ -82,7 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookmanager.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -92,7 +90,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -112,13 +109,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 # 设置语言
-LANGUAGE_CODE = 'zh-Hans' # 'en-us'
+LANGUAGE_CODE = 'zh-Hans'  # 'en-us'
 # 设置时区
-TIME_ZONE = 'Asia/Shanghai' # 'UTC'
+TIME_ZONE = 'Asia/Shanghai'  # 'UTC'
 
 USE_I18N = True
 
@@ -126,8 +122,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# 浏览器访问的路径地址
 STATIC_URL = '/static/'
+# 指定静态资源路径
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
